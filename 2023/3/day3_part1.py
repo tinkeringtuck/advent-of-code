317cd5d897119efc3d3f4d5f800cdf287e2a72cd
Jesse Tuck <tinkeringtuck@gmail.com>
2023-12-13 09:31:43 -0500

    day 3 part 1, including start position for str.find()

--- 2023/3/day3_part1.py
+++ 2023/3/day3_part1.py
@@ -1,24 +1,24 @@
 with open("input.txt", "r") as input_stream:
     data = input_stream.read()
 
-data = """.......5......
-..7*..*.....4*
-...*13*......9
-.......15.....
-..............
-..............
-..............
-..............
-..............
-..............
-21............
-...*9........."""
+# data = """.......5......
+# ..7*..*.....4*
+# ...*13*......9
+# .......15.....
+# ..............
+# ..............
+# ..............
+# ..............
+# ..............
+# ..............
+# 21............
+# ...*9........."""
 
-# need to add check for case like ...*13*...
 
 print(f"{data}\n")
 data = data.split("\n")
 numbers = []
+found_numbers = {}  # found numbers, row index and char index as key and number as value
 
 def find_adjacent(row, symbol_index):
     # knowing symbol index, look for adjacency
@@ -28,17 +28,17 @@ def find_adjacent(row, symbol_index):
             temp = list(row)
             temp[char_index] = "."
             row = ''.join(temp)
-    adjacent_parts = []
     row_parts = [x for x in row.split(".") if x and x.isdigit()]
-    for part in row_parts:
-        part_start = row.find(part)
-        part_end = row.find(part) + len(part) - 1
+    for j, part in enumerate(row_parts):
+        if j == 0:
+            last_index = 0
+        part_start = row.find(part, last_index)
+        part_end = row.find(part, last_index) + len(part) - 1
+        last_index = part_end + 1  # for single digit case and same number on line multiple times
         if part_start != -1:
             if abs(symbol_index - part_start) <= 1 or abs(symbol_index - part_end) <= 1:
                 print(f"adjacent part {part} found")
-                adjacent_parts.append(int(part))
-    return adjacent_parts
-
+                found_numbers[f"{row}_{part_start}"] = int(part)
 def find_symbols(row):
     # find symbol(s) and return index(s)
     symbols = []
@@ -57,9 +57,9 @@ for i, current_row in enumerate(data):
             print(f"current row: \t{current_row}")
             print(f"next row: \t{next_row}")
             print(f"checking current row for symbol at index {symbol_index}")
-            numbers.extend(find_adjacent(current_row, symbol_index))
+            find_adjacent(current_row, symbol_index)
             print(f"checking next row for symbol at index {symbol_index}")
-            numbers.extend(find_adjacent(next_row, symbol_index))
+            find_adjacent(next_row, symbol_index)
     elif i < len(data) - 1:
         # check current, next, and previous
         next_row = data[i+1]
@@ -69,11 +69,11 @@ for i, current_row in enumerate(data):
             print(f"current row: \t{current_row}")
             print(f"next row: \t{next_row}")
             print(f"checking prev row for symbol at index {symbol_index}")
-            numbers.extend(find_adjacent(prev_row, symbol_index))
+            find_adjacent(prev_row, symbol_index)
             print(f"checking current row for symbol at index {symbol_index}")
-            numbers.extend(find_adjacent(current_row, symbol_index))
+            find_adjacent(current_row, symbol_index)
             print(f"checking next row for symbol at index {symbol_index}")
-            numbers.extend(find_adjacent(next_row, symbol_index))
+            find_adjacent(next_row, symbol_index)
     else:
         # check current and previous
         prev_row = data[i-1]
@@ -81,9 +81,8 @@ for i, current_row in enumerate(data):
             print(f"prev row: \t{prev_row}")
             print(f"current row: \t{current_row}")
             print(f"checking prev row for symbol at index {symbol_index}")
-            numbers.extend(find_adjacent(prev_row, symbol_index))
+            find_adjacent(prev_row, symbol_index)
             print(f"checking current row for symbol at index {symbol_index}")
-            numbers.extend(find_adjacent(current_row, symbol_index))
+            find_adjacent(current_row, symbol_index)
 
-# print(f"part numbers found: {numbers}")
-print(f"sum of numbers is {sum(numbers)}")
+print(f"sum is {sum(found_numbers.values())}")
