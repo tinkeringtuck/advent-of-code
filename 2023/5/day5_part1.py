885697e71c4656219a05a70cdff92aad08a65ca3
Jesse Tuck <tinkeringtuck@gmail.com>
2023-12-14 14:24:02 -0500

    day 5 part 1 complete

--- 2023/5/day5_part1.py
+++ 2023/5/day5_part1.py
@@ -3,49 +3,49 @@ from rich.pretty import pprint
 with open("input.txt", "r") as input_stream:
     data = input_stream.read()
 
-data = """seeds: 79 14 55 13
+# data = """seeds: 79 14 55 13
 
-seed-to-soil map:
-50 98 2
-52 50 48
+# seed-to-soil map:
+# 50 98 2
+# 52 50 48
 
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
+# soil-to-fertilizer map:
+# 0 15 37
+# 37 52 2
+# 39 0 15
 
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
+# fertilizer-to-water map:
+# 49 53 8
+# 0 11 42
+# 42 0 7
+# 57 7 4
 
-water-to-light map:
-88 18 7
-18 25 70
+# water-to-light map:
+# 88 18 7
+# 18 25 70
 
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
+# light-to-temperature map:
+# 45 77 23
+# 81 45 19
+# 68 64 13
 
-temperature-to-humidity map:
-0 69 1
-1 0 69
+# temperature-to-humidity map:
+# 0 69 1
+# 1 0 69
 
-humidity-to-location map:
-60 56 37
-56 93 4"""
+# humidity-to-location map:
+# 60 56 37
+# 56 93 4"""
 
 almanac = {
     "seeds": [],
-    "seed-to-soil": {"parent": "seeds"},
-    "soil-to-fertilizer": {"parent": "seed-to-soil"},
-    "fertilizer-to-water": {"parent": "fertilizer-to-water"},
-    "water-to-light": {"parent": "fertilizer-to-water"},
-    "light-to-temperature": {"parent": "water-to-light"},
-    "temperature-to-humidity": {"parent": "light-to-temperature"},
-    "humidity-to-location": {"parent": "temperature-to-humidity"}
+    "seed-to-soil": {},
+    "soil-to-fertilizer": {},
+    "fertilizer-to-water": {},
+    "water-to-light": {},
+    "light-to-temperature": {},
+    "temperature-to-humidity": {},
+    "humidity-to-location": {}
 }
 
 map_type = ""
@@ -53,13 +53,16 @@ for k, each in enumerate(data.split("\n")):
     if each.find("seeds") != -1:
         seeds = [int(x) for x in each.split(":")[1].split(" ") if x.isdigit()]
         almanac["seeds"].extend(seeds)
+        important_numbers = set(seeds)
         print(f"seeds are {almanac.get('seeds')}")
     elif each.find("map") != -1:
+        temp = set()
         map_type = each.split(" ")[0]
         from_type = map_type.split("-")[0]
         to_type = map_type.split("-")[-1]
         print(f"{map_type} found")
     elif not each and map_type:
+        important_numbers = temp
         print(f"{map_type} ended")
     elif not each and not map_type:
         pass # do nothing
@@ -69,13 +72,15 @@ for k, each in enumerate(data.split("\n")):
         destination = numbers[0]
         source = numbers[1]
         num_range = numbers[2]
-        source_nums = [x for x in range(source, source+num_range)]
-        destination_nums = [x for x in range(destination, destination+num_range)]
-        temp_map = {k:v for (k,v) in zip(source_nums, destination_nums)}
-        # temp_map = {range(source, source+num_range), range(destination, destination+num_range)}
-        almanac[map_type].update(temp_map)
-        print(f"added {numbers} for {map_type}")
-        print(f"the {map_type} map is {temp_map}")
+        for important in important_numbers:
+            if important >= source and important <= source + num_range: # number is in range
+                difference = important - source
+                keep_destination = destination + difference
+                print(f"source {important} and destination {keep_destination} for {map_type}")
+                almanac[map_type].update({important: keep_destination})
+                temp.add(keep_destination) # found in range
+            else:
+                temp.add(important) # must be same number  
 
 pprint(almanac, expand_all=True)
 
