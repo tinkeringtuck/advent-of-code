d08e8986086c26f7cd6e07134548748d3d2563e0
Jesse Tuck <tinkeringtuck@gmail.com>
2023-12-14 21:51:10 -0500

    day 6 part 1

---
+++ 2023/6/day6_part1.py
@@ -0,0 +1,34 @@
+with open("input.txt", "r") as input_stream:
+    data = input_stream.read()
+
+# data = """Time:      7  15   30
+# Distance:  9  40  200"""
+
+
+times = []
+distances = []
+for k, each in enumerate(data.split("\n")):
+    numbers = [int(x) for x in each.split(":")[1].split(" ") if x.isdigit()]
+    if k % 2 == 0:
+        times.extend(numbers)
+    else:
+        distances.extend(numbers)
+
+winners = []
+for time, distance in zip(times, distances):
+    ranges = range(1, time)
+    for each in ranges:
+        time_left = time - each
+        traveled = time_left * each
+        if traveled > distance:
+            least = each
+            most = time - each
+            num_winners = time - each - each + 1
+            print(f"{num_winners} winning numbers")
+            winners.append(num_winners)
+            break
+result = 1
+for each in winners:
+    result = result * each
+
+print(f"result is {result}")
